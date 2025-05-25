72d6809f744d72b3f5005cdb86ee496593483baa
Aamir iqbal h <aamiriqbalhafiz06@gmail.com>
2025-05-25 10:58:32 +0530

    changes

--- db.py
+++ db.py
@@ -8,8 +8,16 @@ db_host="localhost"
 def tasklist_db():
     connect = psycopg2.connect(dbname=db_name, user=db_user, password=db_password, host=db_host)
     cur = connect.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
-    cur.execute('SELECT task_name, due_date, is_active FROM task_list')
+    cur.execute('SELECT task_name, due_date, is_done FROM task_list')
     tasklist=cur.fetchall()
     cur.close()
     connect.close()
     return tasklist
+
+def add_task(task,date):
+    connect = psycopg2.connect(dbname=db_name, user=db_user, password=db_password, host=db_host)
+    cur = connect.cursor()
+    cur.execute('INSERT INTO task_list (task_name, due_date) VALUES (%s, %s)', (task,date))
+    connect.commit()
+    cur.close()
+    connect.close() 
